cd0379beb2849dba5300a0ab791d9745fd020f4a
bnonni <bryanwnonni@gmail.com>
2019-07-12 11:17:34 -0400

    doctor folder => contains shell scripts to debug

---
+++ app/doctor/iOS_Tools.sh
@@ -0,0 +1,8 @@
+brew update;
+brew uninstall --ignore-dependencies libimobiledevice;
+brew uninstall --ignore-dependencies usbmuxd;
+brew install --HEAD usbmuxd;
+brew unlink usbmuxd;
+brew link usbmuxd;
+brew install --HEAD libimobiledevice;
+brew install ideviceinstaller
